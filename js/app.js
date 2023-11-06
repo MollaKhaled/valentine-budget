a9fc7b7cec7d59163e0cd589dd6db67f70798cb0
Mollakhaled <khaledbalok@gmail.com>
2023-11-06 11:37:42 +0600

    Total Sum Update

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
-document.getElementById("kitkat-buy-btn").addEventListener('click',function(){
-   const quantity = document.getElementById("kitkat-quantity").value;
-   const kitkatCost = quantity * 200;
-   setInnerText("chocolate", kitkatCost);
+document.getElementById("kitkat-buy-btn").addEventListener('click', function () {
+  const quantity = inputFieldValue('kitkat-quantity');
+  const kitkatCost = quantity * 200;
+  setInnerText("chocolate", kitkatCost);
+  total();
 })
-document.getElementById("rose-buy-btn").addEventListener('click',function(){
-   const quantity = document.getElementById("rose-quantity").value;
-   const roseCost = quantity * 200;
-   setInnerText("rose", roseCost);
+document.getElementById("rose-buy-btn").addEventListener('click', function () {
+  const quantity = inputFieldValue('rose-quantity')
+  const roseCost = quantity * 100;
+  setInnerText("rose", roseCost);
+  total();
 })
-document.getElementById("diary-buy-btn").addEventListener('click',function(){
-   const quantity = document.getElementById("diary-quantity").value;
-   const diaryCost = quantity * 200;
-   setInnerText("diary", diaryCost);
+document.getElementById("diary-buy-btn").addEventListener('click', function () {
+  const quantity = inputFieldValue("diary-quantity");
+  const diaryCost = quantity * 300;
+  setInnerText("diary", diaryCost);
+  total();
 })
 
-function setInnerText(id, value){
+function setInnerText(id, value) {
   document.getElementById(id).innerText = value;
+}
+function inputFieldValue(id) {
+  const value = document.getElementById(id).value;
+  return parseInt(value);
+}
+
+function total() {
+  const chocolate = document.getElementById("chocolate").innerText;
+  const rose = document.getElementById("rose").innerText;
+  const diary = document.getElementById("diary").innerText;
+  const totalSum = parseInt(chocolate) + parseInt(rose) + parseInt(diary);
+  setInnerText("total", totalSum);
+
 }
